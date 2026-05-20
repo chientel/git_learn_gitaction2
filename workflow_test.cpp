c44baf728d8237cdf34bcef15beadb304bef8a5d
chientel <nguyenchiengthuy02@gmail.com>
2026-05-20 19:32:50 +0700

    Add workflow test C++ file

---
+++ workflow_test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+constexpr int EXIT_SUCCESS_CODE = 0;
+
+void printWorkflowStatus()
+{
+    const char* MESSAGE = "GitHub Actions workflow test";
+    std::cout << MESSAGE << '\n';
+}
+
+int main()
+{
+    printWorkflowStatus();
+    return EXIT_SUCCESS_CODE;
+}
